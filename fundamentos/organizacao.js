a078146547d7e1d56ad18aaa91ef97f2b5d66979
Aruhan Mathias <aruhanmathias@hotmail.com>
2021-08-16 20:35:50 -0300

    Fundamentos JS

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log('Aôba!');
+
+{
+    {
+        console.log('Sentença 02 sem ponto e vírgula')
+        console.log('Sentença 03 sem ponto e vírgula')
+    }
+}
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let tipoFraca = 'Olá!';
+console.log(tipoFraca);
+console.log(typeof tipoFraca);// Aqui o tipo é string
+
+tipoFraca = 3.1516;
+console.log(tipoFraca);
+console.log(typeof tipoFraca); // Mudou o tipo para number
+
+// Evitar nomes genéricos/siglas
+
+let valor = '';
+let numero = 1;
+let retornoFuncao = false 
